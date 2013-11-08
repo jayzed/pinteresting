c0da915ae6c7f9d6170e8e76cb85794714f1a4d1
Jordy Zagdanski <jayzed@gmail.com>
2013-11-07 23:17:48 -0500

    Adding devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  #Heroku
+  config.action_mailer.default_url_options = { :host => 'http://lit-forest-7693.herokuapp.com/' }
 end
